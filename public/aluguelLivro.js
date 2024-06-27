3f1a2cec0ca31d3a1df5ef9f659a5393bca52b28
LuizNicoletti <luiznicoletti101001@gmail.com>
2024-06-27 00:25:30 -0300

    testes

--- public/aluguelLivro.js
+++ public/aluguelLivro.js
@@ -1,48 +1,34 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector("form");
-    
-    form.addEventListener("submit", async (event) => {
-        event.preventDefault();
-        
-        const nomeLivro = form.nome.value;
-        const idLivro = form.username.value;
-        const autor = form.password.value;
-        const cpfCliente = form.password.value;
-        const data = form.password.value;
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
 
-        // Verificação dos campos
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const idLivro = form.elements['username'].value;
+        const cpfCliente = form.elements['password'].value;
+        const data = form.elements['password'].value;
+
         try {
-            const response = await fetch('/api/verify', {
+            const response = await fetch('/alugueis', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json',
+                    'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ nomeLivro, idLivro, autor, cpfCliente })
+                body: JSON.stringify({ idLivro, cpfCliente, data })
             });
 
             const result = await response.json();
 
-            if (result.error) {
-                alert(result.error);
+            if (response.ok) {
+                alert('Empréstimo criado com sucesso');
             } else {
-                const response = await fetch('/api/aluguel', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ nomeLivro, idLivro, autor, cpfCliente, data })
-                });
-                
-                if (response.ok) {
-                    alert('Empréstimo cadastrado com sucesso');
-                } else {
-                    alert('Erro ao cadastrar o empréstimo');
-                }
+                alert(`Erro: ${result.error}`);
             }
         } catch (error) {
             console.error('Erro:', error);
-            alert('Erro ao verificar os dados do formulário');
+            alert('Erro ao criar empréstimo');
         }
     });
 });
+
 
